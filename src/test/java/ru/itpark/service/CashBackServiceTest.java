b2bf6d3a98a3e0ae12e6b8997640a41d1f5930d7
student666 <student666@mail.ru>
2018-12-02 16:21:04 +0300

    message

--- src/test/java/ru/itpark/service/CashBackServiceTest.java
+++ src/test/java/ru/itpark/service/CashBackServiceTest.java
@@ -1,5 +1,6 @@
 package ru.itpark.service;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +8,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class CashBackServiceTest {
 
     @Test
-    void cashback() {
+    @Disabled("should calculate cashback for under imit amount")
+    void cashbackUnderLimit() {
+        CashBackService service=new CashBackService();
+        int result = service.cashback(1_000);
+        assertEquals(50,result);
     }
 }
